a24d5b1781eef644719b8723367b7963fe9a553a
Divesh Gupta <diveshg9@gmail.com>
2025-02-08 10:09:59 -0500

    update notif disable

--- Assets/Scripts/DisableSelf.cs
+++ Assets/Scripts/DisableSelf.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 
 public class DisableSelf : MonoBehaviour
 {
-    public GameObject objectToDisable;
     public void DisableMe()
     {
-        objectToDisable.SetActive(false);
+        gameObject.SetActive(false);
     }
 }
